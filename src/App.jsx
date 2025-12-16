05da6d646ace888a1abd68b5a0cff31681790055
Hari Sai Kaja <harisai@Haris-MacBook-Air.local>
2025-12-15 21:38:22 -0500

    Initialized vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hari Sai Kaja</h1>
+      <p>
+        I’m Hari Sai Kaja, a full-stack and data-driven software engineer currently pursuing my Master’s in Applied Computer Science at GVSU. 
+        I specialize in building scalable web applications, secure systems, and data-centric solutions with real-world impact.
+      </p>
+    </div>
+  );
+}
+
+export default App;
