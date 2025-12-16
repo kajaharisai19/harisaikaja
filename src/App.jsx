3eb592bc9bdc0b3c3cc4a82020715d3c926f1026
Hari Sai Kaja <harisai@Haris-MacBook-Air.local>
2025-12-15 23:41:09 -0500

    Added Tailwind and centered content in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Hari Sai Kaja</h1>
-      <p>
+    <div className="min-h-screen flex items-center justify-center px-6">
+      <div className="text-center max-w-2xl">
+        <h1 className="text-4xl font-bold tracking-tight">Hari Sai Kaja</h1>
+        <p className="mt-4 text-lg text-gray-600">
         I’m Hari Sai Kaja, a full-stack and data-driven software engineer currently pursuing my Master’s in Applied Computer Science at GVSU. 
         I specialize in building scalable web applications, secure systems, and data-centric solutions with real-world impact.
-      </p>
+        </p>
+      </div>
     </div>
   );
 }
